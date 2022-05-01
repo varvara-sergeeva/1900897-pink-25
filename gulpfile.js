eeb4175b910187a8c9c692218c74d9c5e2ee910c
varvara-sergeeva <raunuperetemheru@gmail.com>
2022-05-02 01:32:10 +0300

    доработка сетки на главной странице. оформление элементов интерфейса

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@ const server = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
