8d8d6488ba2e0a8c8d6f95f3665211c8cb0a9cea
varvara-sergeeva <raunuperetemheru@gmail.com>
2022-06-18 23:15:41 +0300

    Слайдер на главной странице + лайки в фотогалерее

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// бургерное меню
 let burgerToggle = document.querySelectorAll('.navigation__toggle');
 let burgerMenu = document.querySelector('.navigation__menu');
 let header = document.querySelector('.header');
@@ -16,7 +17,78 @@ burgerToggle.forEach(function (item) {
 
 map.classList.remove('nojs');
 
-//В доработке
+// слайдер для отзывов
+const prev = document.querySelector('.reviews__previous');
+const next = document.querySelector('.reviews__next');
+const slides = document.querySelectorAll('.reviews__item.slider__item');
+const toggles = document.querySelectorAll('.reviews__toggles > .slider__toggle');
+let index = 0;
+
+const currentSlide = n => {
+  for(let slide of slides) {
+    slide.classList.remove('slider__item--current');
+  }
+  slides[n].classList.add('slider__item--current');
+}
+
+const currentToggle = n => {
+  for(let dot of toggles) {
+    dot.classList.remove('slider__toggle--current');
+  }
+  toggles[n].classList.add('slider__toggle--current');
+}
+
+const nextSlide = () => {
+  if(index == slides.length - 1) {
+    index = 0;
+    currentSlide(index);
+    currentToggle(index);
+  } else {
+    index++;
+    currentSlide(index);
+    currentToggle(index);
+  }
+}
+
+const prevSlide = () => {
+  if(index == 0) {
+    index = slides.length - 1;
+    currentSlide(index);
+    currentToggle(index);
+  } else {
+    index--;
+    currentSlide(index);
+    currentToggle(index);
+  }
+}
+
+toggles.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    currentSlide(index);
+    currentToggle(index);
+  })
+})
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+//Лайки для галереи
+
+let like = document.querySelectorAll(".photo__like-link");
+let likesNumber = document.querySelectorAll(".photo__like-number");
+
+like.onclick = function (evt) {
+  evt.preventDefault();
+  if (like.classList.contains('activelike')) {
+    likesNumber.textContent--;
+  } else {
+    likesNumber.textContent++;
+  }
+  like.classList.toggle('activelike');
+};
+
+//POP-UP в доработке
 let popup = document.querySelectorAll(".popup");
 let error = document.querySelector(".popup--error");
 let confirm = document.querySelector(".popup--confirm");
